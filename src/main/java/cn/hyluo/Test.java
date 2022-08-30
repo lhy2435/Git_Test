281068d8e802c141ad948174fe2e3c3e9da56db1
hyluo <hyluo@smit.com>
2022-08-30 20:28:29 +0800

    second commit

--- src/main/java/cn/hyluo/Test.java
+++ src/main/java/cn/hyluo/Test.java
@@ -3,5 +3,6 @@ package cn.hyluo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("commit 22222222");
     }
 }
